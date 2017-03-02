a30185d5ea97f02fc965f1a284a6608265478a98
Jaiana Dixon <jdixon@wlapcs.org>
2017-03-02 03:13:45 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+    createCanvas(600,400);
+    background(242,29,164);
+}
+
+function draw(){
+    line(35,80,400,200)
+    line(-20,80,400,200)
+    
+    stroke(255,255,255);
+    fill(4, 89,226)
+    ellipse(520,250,80,90)
+    
+    fill(255, 89, 0)
+    rect(230,125,230,220);
+     
+    stroke(80,20,0)
+    strokeWeight(7);
+    fill(0,255,0);
+    ellipse(50,250,80,80)
+    
+}
